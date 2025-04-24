ae1f9b7d36957edab45bf142607b2806accec645
RngAd33 <yxbsyx@outlook.com>
2025-04-24 22:26:08 +0800

    changing

--- src/main/java/com/yupi/web/model/dto/generator/GeneratorAddRequest.java
+++ src/main/java/com/yupi/web/model/dto/generator/GeneratorAddRequest.java
@@ -1,7 +1,14 @@
 package com.yupi.web.model.dto.generator;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.yupi.web.meta.Meta;
 import lombok.Data;
 
 /**
@@ -11,19 +18,88 @@ import lombok.Data;
 public class GeneratorAddRequest implements Serializable {
 
     /**
-     * 标题
+     * id
      */
-    private String title;
+    @TableId(type = IdType.AUTO)
+    private Long id;
 
     /**
-     * 内容
+     * 名称
      */
-    private String content;
+    private String name;
 
     /**
-     * 标签列表
+     * 描述
+     */
+    private String description;
+
+    /**
+     * 基础包
+     */
+    private String basePackage;
+
+    /**
+     * 版本
+     */
+    private String version;
+
+    /**
+     * 作者
+     */
+    private String author;
+
+    /**
+     * 标签列表（json 数组）
      */
     private List<String> tags;
 
+    /**
+     * 图片
+     */
+    private String picture;
+
+    /**
+     * 文件配置（json字符串）
+     */
+    private Meta.FileConfig fileConfig;
+
+    /**
+     * 模型配置（json字符串）
+     */
+    private Meta.ModelConfig modelConfig;
+
+    /**
+     * 代码生成器产物路径
+     */
+    private String distPath;
+
+    /**
+     * 状态
+     */
+    private Integer status;
+
+    /**
+     * 创建用户 id
+     */
+    private Long userId;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+    /**
+     * 是否删除
+     */
+    @TableLogic
+    private Integer isDelete;
+
+    @TableField(exist = false)
     private static final long serialVersionUID = 1L;
+
 }
--- src/main/java/com/yupi/web/model/dto/generator/GeneratorUpdateRequest.java
+++ src/main/java/com/yupi/web/model/dto/generator/GeneratorUpdateRequest.java
@@ -1,7 +1,14 @@
 package com.yupi.web.model.dto.generator;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.yupi.web.meta.Meta;
 import lombok.Data;
 
 /**
@@ -13,22 +20,86 @@ public class GeneratorUpdateRequest implements Serializable {
     /**
      * id
      */
+    @TableId(type = IdType.AUTO)
     private Long id;
 
     /**
-     * 标题
+     * 名称
      */
-    private String title;
+    private String name;
 
     /**
-     * 内容
+     * 描述
      */
-    private String content;
+    private String description;
 
     /**
-     * 标签列表
+     * 基础包
+     */
+    private String basePackage;
+
+    /**
+     * 版本
+     */
+    private String version;
+
+    /**
+     * 作者
+     */
+    private String author;
+
+    /**
+     * 标签列表（json 数组）
      */
     private List<String> tags;
 
+    /**
+     * 图片
+     */
+    private String picture;
+
+    /**
+     * 文件配置（json字符串）
+     */
+    private Meta.FileConfig fileConfig;
+
+    /**
+     * 模型配置（json字符串）
+     */
+    private Meta.ModelConfig modelConfig;
+
+    /**
+     * 代码生成器产物路径
+     */
+    private String distPath;
+
+    /**
+     * 状态
+     */
+    private Integer status;
+
+    /**
+     * 创建用户 id
+     */
+    private Long userId;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+    /**
+     * 是否删除
+     */
+    @TableLogic
+    private Integer isDelete;
+
+    @TableField(exist = false)
     private static final long serialVersionUID = 1L;
+
 }
