aca61486797948fd9582748b645935c973c2410a
RngAd33 <yxbsyx@outlook.com>
2025-04-24 22:25:37 +0800

    changing

---
+++ src/main/java/com/yupi/web/service/GeneratorService.java
@@ -0,0 +1,15 @@
+package com.yupi.web.service;
+
+import com.yupi.web.model.entity.Generator;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * 代码生成器服务接口
+ *
+ * @author Dr.YX
+ * @description 针对表【generator(代码生成器)】的数据库操作Service
+ * @createDate 2025-04-24 22:17:20
+ */
+public interface GeneratorService extends IService<Generator> {
+
+}
---
+++ src/main/java/com/yupi/web/service/impl/GeneratorServiceImpl.java
@@ -0,0 +1,24 @@
+package com.yupi.web.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.yupi.web.model.entity.Generator;
+import com.yupi.web.service.GeneratorService;
+import com.yupi.web.mapper.GeneratorMapper;
+import org.springframework.stereotype.Service;
+
+/**
+ * 代码生成器服务实现类
+ *
+ * @author Dr.YX
+ * @description 针对表【generator(代码生成器)】的数据库操作Service实现
+ * @createDate 2025-04-24 22:17:20
+ */
+@Service
+public class GeneratorServiceImpl extends ServiceImpl<GeneratorMapper, Generator>
+    implements GeneratorService{
+
+}
+
+
+
+
