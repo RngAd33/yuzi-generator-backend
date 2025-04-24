90872e2bb0c15a34fa914f007855d2c0baae6075
RngAd33 <yxbsyx@outlook.com>
2025-04-24 17:21:23 +0800

    changing

--- src/main/java/com/yupi/web/model/entity/Generator.java
+++ src/main/java/com/yupi/web/model/entity/Generator.java
@@ -1,8 +1,7 @@
 package com.yupi.web.model.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.*;
+
 import java.util.Date;
 import lombok.Data;
 
@@ -53,5 +52,9 @@ public class Generator {
     /**
      * 是否删除
      */
+    @TableLogic
     private Integer isDelete;
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
 }
