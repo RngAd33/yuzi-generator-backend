97094b4231332d7f34fc3a2c5f341fa67ac965d3
RngAd33 <yxbsyx@outlook.com>
2025-04-24 17:19:07 +0800

    changing

---
+++ src/main/java/com/yupi/web/service/GeneratorService.java
@@ -0,0 +1,13 @@
+package com.yupi.web.service;
+
+import com.yupi.web.model.entity.Generator;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author Dr.YX
+* @description 针对表【generator(代码生成器)】的数据库操作Service
+* @createDate 2025-04-24 17:16:42
+*/
+public interface GeneratorService extends IService<Generator> {
+
+}
